04fe3d8751ec509004eeabebe29c3c239e6c88dd
Arseny Sokolov <me@arsen.pw>
2021-06-24 18:30:10 +0800

    Добавлен правильный фильтр по санаторию.

--- vouchers.py
+++ vouchers.py
@@ -557,7 +557,7 @@ class Voucher(object):
         if self.voucher_url:
             filters = {
                 'order_by': '-number',
-                'sanatorium_id': self.sanatorium_id,
+                'f': f'sanatorium_id={self.sanatorium_id}',
                 'limit': 1,
             }
             url = urljoin(self.voucher_url, '/api/v1.0/voucher/')
